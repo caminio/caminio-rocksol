347dc4479b746ed5c8c158db21c77d526ba23389
David Reinisch <david.reinisch@tastenwerk.com>
2014-03-21 00:33:17 +0100

    changed Pepples

--- api/models/pebble.js
+++ api/models/pebble.js
@@ -1,3 +1,20 @@
+/** 
+ * @Author: David Reinisch
+ * @Company: TASTENWERK e.U.
+ * @Copyright: 2014 by TASTENWERK
+ * @License: Commercial
+ *
+ * @Date:   2014-03-21 00:30:53
+ *
+ * @Last Modified by:   David Reinisch
+ * @Last Modified time: 2014-03-21 00:30:59
+ *
+ * This source code is not part of the public domain
+ * If server side nodejs, it is intendet to be read by
+ * authorized staff, collaborator or legal partner of
+ * TASTENWERK only
+ */
+
 /**
  *
  * @class Pebble
@@ -17,6 +34,12 @@ module.exports = function Pebble( caminio, mongoose ){
      * @type String
      */  
     name: { type: String, public: true },
+
+    /**
+     * @property type
+     * @type String
+     */
+    type: { type: String, enum: [ 'table', 'gallery', 'page' ], public: true },
 
     /**
      * @property translations
--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,5 @@ module.exports.routes = {
   '/caminio/websites': 'WebsitesController#index',
   '/caminio/website/available_layouts': 'WebsitesController#available_layouts',
   'autorest /caminio/webpages': 'Webpage',
+  'autorest /caminio/pepples': 'Pepple',
 };

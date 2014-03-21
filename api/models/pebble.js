c2367b2b6bf932487a72d36bf2d4fb1094c6cf71
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-03-21 09:52:16 +0100

    pebble uncommented, as it avoided to start the server

--- api/models/pebble.js
+++ api/models/pebble.js
@@ -6,8 +6,8 @@
  *
  * @Date:   2014-03-21 00:30:53
  *
- * @Last Modified by:   David Reinisch
- * @Last Modified time: 2014-03-21 00:30:59
+ * @Last Modified by:   thorsten zerha
+ * @Last Modified time: 2014-03-21 09:50:32
  *
  * This source code is not part of the public domain
  * If server side nodejs, it is intendet to be read by

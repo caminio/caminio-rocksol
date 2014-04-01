5108d0a2657872792675c8077b4513922e66664b
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-04-01 13:17:26 +0200

    reparied contentPath in test

--- test/support/app/config/environments/test.js
+++ test/support/app/config/environments/test.js
@@ -3,6 +3,8 @@ module.exports = {
   db: {
     url: 'mongodb://localhost:27017/caminio-rocksol-test',
     debug: false
-  }
+  },
+
+  contentPath: __dirname+'/../../../content'
 
 };
--- test/support/app/config/site.js
+++ test/support/app/config/site.js
@@ -1,4 +1,3 @@
 module.exports.site = {
-  name: 'test-rocksol',
-  contentPath: 'test/support/content'
+  name: 'test-rocksol'
 };

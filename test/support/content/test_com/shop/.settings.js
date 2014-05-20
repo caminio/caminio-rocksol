658d17295784b6c0e16cea8f210d5a18a60f9fbd
David Reinisch <david.reinisch@tastenwerk.com>
2014-05-20 20:41:54 +0200

    commit for thorsten

--- test/support/content/test_com/shop/.settings.js
+++ test/support/content/test_com/shop/.settings.js
@@ -7,7 +7,7 @@
  * @Date:   2014-05-20 13:10:45
  *
  * @Last Modified by:   David Reinisch
- * @Last Modified time: 2014-05-20 16:03:43
+ * @Last Modified time: 2014-05-20 16:58:34
  *
  * This source code is not part of the public domain
  * If server side nodejs, it is intendet to be read by

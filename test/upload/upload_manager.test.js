0643ab5667eff4b7559e2eea1e02cd1f9e206dec
David Reinisch <david.reinisch@tastenwerk.com>
2014-05-20 11:22:13 +0200

    temporarly switched to only implementing the file upload for the shop

---
+++ test/upload/upload_manager.test.js
@@ -0,0 +1,27 @@
+/** 
+ * @Author: David Reinisch
+ * @Company: TASTENWERK e.U.
+ * @Copyright: 2014 by TASTENWERK
+ * @License: Commercial
+ *
+ * @Date:   2014-05-20 11:19:55
+ *
+ * @Last Modified by:   David Reinisch
+ * @Last Modified time: 2014-05-20 11:21:25
+ *
+ * This source code is not part of the public domain
+ * If server side nodejs, it is intendet to be read by
+ * authorized staff, collaborator or legal partner of
+ * TASTENWERK only
+ */
+
+var uploadManager = require('./../../lib/upload/upload_manager');
+
+describe('upload manager test', function() {
+
+  it('is there', function(){
+    console.log(uploadManager);
+
+  });
+
+});

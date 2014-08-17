00bf106d634fd2bf1e368dfbcce0873bb24d8dd1
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-08-17 18:58:50 +0200

    morris removed

--- assets/javascripts/caminio-rocksol/views/stats_visits_view.js
+++ assets/javascripts/caminio-rocksol/views/stats_visits_view.js
@@ -16,15 +16,15 @@
         { date: '2014-06-15', unique: 18, views: 30 },
         { date: '2014-06-16', unique: 5, views: 7 }
       ];
-      Morris.Line({
-        element: this.$().get(0),
-        data: visits,
-        hideHover: 'auto',
-        xkey: 'date',
-        ykeys: ['unique', 'views' ],
-        dateFormat: function (x) { return new moment(x).format('dddd'); },
-        labels: [ Em.I18n.t('stats.unique'), Em.I18n.t('stats.views')]
-      });
+      // Morris.Line({
+      //   element: this.$().get(0),
+      //   data: visits,
+      //   hideHover: 'auto',
+      //   xkey: 'date',
+      //   ykeys: ['unique', 'views' ],
+      //   dateFormat: function (x) { return new moment(x).format('dddd'); },
+      //   labels: [ Em.I18n.t('stats.unique'), Em.I18n.t('stats.views')]
+      // });
 
     }
 
